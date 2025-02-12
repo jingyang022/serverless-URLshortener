d34a36b2ab03bcaeaf6ceb1921f161c0a1a2430d
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-12 13:39:04 +0800

    added files

--- lambda.tf
+++ lambda.tf
@@ -3,15 +3,15 @@
 #############################################################
 
 # Define an archive_file datasource that creates the lambda archive
-data "archive_file" "lambda" {
+data "archive_file" "lambda-post-file" {
  type        = "zip"
  #source_file = "hello.py"
  source_dir  = "${path.module}/create-url"
  output_path = "${path.module}/create-url/create-url-lambda.zip"
 }
 
-resource "aws_lambda_function" "lambda-post" {
- function_name = "yap-lambda-post"
+resource "aws_lambda_function" "lambda-post-func" {
+ function_name = "yap-lambda-post-func"
  role          = aws_iam_role.lambda_exec_role.arn
  handler       = "lambda_function.lambda_handler"
  runtime       = "python3.12"
@@ -34,3 +34,32 @@ resource "aws_cloudwatch_log_group" "lambda-post-logs" {
 #############################################################
 # Lambda Function (for GET)
 #############################################################
+
+# Define an archive_file datasource that creates the lambda archive
+data "archive_file" "lambda-get-file" {
+ type        = "zip"
+ #source_file = "hello.py"
+ source_dir  = "${path.module}/retrieve-url"
+ output_path = "${path.module}/retrieve-url/retrieve-url-lambda.zip"
+}
+
+resource "aws_lambda_function" "lambda-get-func" {
+ function_name = "yap-lambda-get-func"
+ role          = aws_iam_role.lambda_exec_role.arn
+ handler       = "lambda_function.lambda_handler"
+ runtime       = "python3.12"
+ filename      = "${path.module}/retrieve-url/retrieve-url-lambda.zip"
+
+  # Environment Variables
+  environment {
+    variables = {
+       	DDB_TABLE = aws_dynamodb_table.url-dynamodb-table.name
+    }
+  }
+}
+
+# aws_cloudwatch_log_group to get the logs of the Lambda execution.
+resource "aws_cloudwatch_log_group" "lambda-get-logs" {
+  name              = "/aws/lambda/yap-lambda-get"
+  retention_in_days = 7
+}
