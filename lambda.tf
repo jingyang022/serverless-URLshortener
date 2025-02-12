6faf382bbcba0d4db85e91efc3805484dd4c4dd4
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-12 20:15:12 +0800

    updated files

--- lambda.tf
+++ lambda.tf
@@ -7,7 +7,7 @@ data "archive_file" "lambda-post-file" {
   type = "zip"
   #source_file = "hello.py"
   source_dir  = "${path.module}/create-url"
-  output_path = "${path.module}/create-url/create-url-lambda.zip"
+  output_path = "${path.module}/create-url/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "lambda-post-func" {
@@ -15,12 +15,16 @@ resource "aws_lambda_function" "lambda-post-func" {
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
-  filename      = "${path.module}/create-url/create-url-lambda.zip"
+  filename      = data.archive_file.lambda-post-file.output_path
 
   # Environment Variables
   environment {
     variables = {
-      DDB_TABLE = aws_dynamodb_table.url-dynamodb-table.name
+      DB_NAME = aws_dynamodb_table.url-dynamodb-table.name
+      APP_URL    = "https://yap-urlshortener.sctp-sandbox.com"
+      MAX_CHAR   = "16"
+      MIN_CHAR   = "12"
+      REGION_AWS = "ap-southeast-1"
     }
   }
 }
@@ -50,7 +54,7 @@ data "archive_file" "lambda-get-file" {
   type = "zip"
   #source_file = "hello.py"
   source_dir  = "${path.module}/retrieve-url"
-  output_path = "${path.module}/retrieve-url/retrieve-url-lambda.zip"
+  output_path = "${path.module}/retrieve-url/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "lambda-get-func" {
@@ -58,12 +62,13 @@ resource "aws_lambda_function" "lambda-get-func" {
   role          = aws_iam_role.lambda_exec_role.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
-  filename      = "${path.module}/retrieve-url/retrieve-url-lambda.zip"
+  filename      = "${path.module}/retrieve-url/lambda_function.zip"
 
   # Environment Variables
   environment {
     variables = {
-      DDB_TABLE = aws_dynamodb_table.url-dynamodb-table.name
+      DB_NAME = aws_dynamodb_table.url-dynamodb-table.name
+      REGION_AWS = "ap-southeast-1"
     }
   }
 }
